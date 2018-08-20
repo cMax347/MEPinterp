c695b6eacd44c6bc4c5e887ba00adf6bb2fa2310
Compiler-max <max.merte@gmail.com>
2018-08-20 14:14:17 +0200

    real and complex interfaces for standard operations

--- src/matrix_math.f90
+++ src/matrix_math.f90
@@ -1,27 +1,44 @@
 module matrix_math
 
-
-
 	implicit none
 
-
-
 	private
-	public						::			zheevr_wrapper, zheevd_wrapper,       & 
-                                            matrix_comm,                          &
-                                            crossP, is_equal_vect
+	public						::			my_Levi_Civita,                         &
+                                            zheevr_wrapper, zheevd_wrapper,         & 
+                                            uni_gauge_trafo,                        &
+                                            crossP,                                 &
+                                            is_equal_vect,                          &
+                                            convert_tens_to_vect,                   &
+                                            matrix_comm                                         
+
 
 
     interface crossP
-        module procedure crossPreal
-        module procedure crossPcplx
+        module procedure real_crossP
+        module procedure cplx_crossP
     end interface crossP
+
+    interface  is_equal_vect
+        module procedure real_is_equal_vect
+        module procedure cplx_is_equal_vect
+    end interface is_equal_vect
+
+    interface convert_tens_to_vect
+        module procedure real_tens_to_vect
+        module procedure cplx_tens_to_vect
+    end interface convert_tens_to_vect
+
+    interface matrix_comm
+        module procedure real_matrix_comm
+        module procedure cplx_matrix_comm
+    end interface matrix_comm
+
+
+
+
 
 
     integer,        parameter   ::  dp              = kind(0.d0)
-
-
-
 
 	contains
 
@@ -29,6 +46,33 @@ module matrix_math
 
 
 !public:
+    integer function my_Levi_Civita(i,j,k)
+        !Hard coded Levi Civita tensor
+        integer,        intent(in)      :: i,j,k
+        logical                         :: even, odd
+        !
+        !
+        even    = (i==1 .and. j==2 .and. k==3) .or. (i==2 .and. j==3 .and. k==1) .or. (i==3 .and. j==1 .and. k==2)
+        odd     = (i==3 .and. j==2 .and. k==1) .or. (i==1 .and. j==3 .and. k==2) .or. (i==2 .and. j==1 .and. k==3)
+        !
+        if(even)        then        
+                                my_Levi_Civita  =  1
+        else if(odd)    then    
+                                my_Levi_Civita  = -1
+        else                
+                                my_Levi_Civita  =  0
+        end if
+        !
+        !DEBUGGING
+        if(even .and. odd) then
+            write(*,*)"[myLeviCivita]: myLeviCivita detected even and odd, contact the programer he fucked up"
+        end if
+        !
+        return
+    end function 
+
+
+
 	subroutine zheevr_wrapper(a, w ,z, m)
     	!https://software.intel.com/en-us/mkl-developer-reference-fortran-heevr#6ADF761A-127A-4C9B-9A2A-1A8AA4602CE1
     	!with ability to solve only selected eigenvalues (& vectors)
@@ -64,15 +108,11 @@ module matrix_math
     	allocate(  work(lwork)	)
     	allocate( rwork(lrwork)	)
     	allocate( iwork(liwork)	)	
-    	
     	!
     	call zheevr(jobz, range, uplo, n, a, lda, vl, vu, il, iu, abstol, &
     					m, w, z, ldz, isuppz, work, lwork, rwork, lrwork, iwork, liwork, info)
     	!
-    	!
     	call errCheck(n,info,jobz)
-    	!
-    	!
     	return
     end subroutine
 
@@ -85,8 +125,7 @@ module matrix_math
 		!https://software.intel.com/en-us/node/469182        
 		complex(dp) , intent(inout)   :: A(:,:)
 		real(dp)    , intent(out)     :: w(:)
-            
-
+            !
             !set up workspace arrays (assuming jobz='V' and using zheevd)
             complex(dp), allocatable, dimension(:)   :: work
             real(dp)   , allocatable, dimension(:)   :: rwork
@@ -103,31 +142,172 @@ module matrix_math
             allocate(  work( lwork) )
             allocate( rwork(lrwork) )
             allocate( iwork(liwork) )
-
+            !
             jobz='V'
             uplo='U' !test this behaviour
-
+            !
             !solve eigenvalue problem
-            !call zheevd(jobz, uplo, n, a,lda, w, work, lwork, rwork, lrwork, iwork, liwork, info)
-             call zheevd(jobz, uplo, n, A, n , w, work, lwork, rwork, lrwork, iwork, liwork, info)
-            
+            call zheevd(jobz, uplo, n, A, n , w, work, lwork, rwork, lrwork, iwork, liwork, info)
+            !
             !check if system was solved correctly
             call errCheck(n,info,jobz)
-            !
-!            if(.true.) then
-!                  call normalCheck(n,A)
-!            end if
-            !
             return
 	end subroutine
 
 
 
+    subroutine uni_gauge_trafo(U_mat, M_mat)
+        !   does a gauge trafo of the gauge-covariant matrix M_mat
+        !   by applying the unitary matrix U_mat as shown in 
+        !   Eq.(21), PRB 74, 195118, (2006)
+        !
+        !   since U is defined differently in the paper and in the lapack
+        !   consider the following
+        !
+        !   want:   M^(hbar)    
+        !   
+        !    paper:      U^*    H^(W)   U   = H^(H)
+        !           and M^(hbar) = U^* M^(W) U
+        !
+        !   lapack:
+        !            U  H^(W)   U^*     = H^(H) 
+        !   therefore here M^(hbar) = U M^(W) U^*
 
-    subroutine  matrix_comm(A,  B,  C)
+        complex(dp),        intent(in)      ::  U_mat(:,:)
+        complex(dp),        intent(inout)   ::  M_mat(:,:)
+        complex(dp),    allocatable         ::  U_dag(:,:)
+        !
+        allocate(       U_dag(  size(U_mat,1),size(U_mat,2) )           )
+        !
+        U_dag   = conjg(    transpose( U_mat )  )
+        !
+        M_mat   = matmul(   M_mat   ,   U_dag   )
+        M_mat   = matmul(   U_mat   ,   M_mat   )
+        !
+        return
+    end subroutine
+
+
+
+
+
+
+!----------------------------------------------------------------------------------------------------------------------
+!----------------------------------------------------------------------------------------------------------------------
+!------------INTERFACES------------------------------------------------------------------------------------------------
+!
+!
+    function real_crossP(a,b)
+        !cross product of two real 3dim vectors a,b
+        real(dp), dimension(3)              :: real_crossP
+        real(dp), dimension(3), intent(in)  :: a, b
+        !
+        real_crossP(1)   =   a(2) * b(3)     -   a(3) * b(2)  
+        real_crossP(2)   =   a(3) * b(1)     -   a(1) * b(3) 
+        real_crossP(3)   =   a(1) * b(2)     -   a(2) * b(1) 
+        !
+        return
+    end function
+
+    function cplx_crossP(a,b)
+        !cross product of two complex 3dim vectors a,b
+        complex(dp)                 :: cplx_crossP(3)
+        complex(dp),    intent(in)  :: a(3), b(3)
+        !
+        cplx_crossP(1)   =   a(2) * b(3)     -   a(3) * b(2)  
+        cplx_crossP(2)   =   a(3) * b(1)     -   a(1) * b(3) 
+        cplx_crossP(3)   =   a(1) * b(2)     -   a(2) * b(1) 
+        !
+        return
+    end function
+!----------------------------------------------------------------------------------------------------------------------
+!
+!
+    logical function real_is_equal_vect(acc, a,b)
+        real(dp),                   intent(in)     ::   acc
+        real(dp),   dimension(3),   intent(in)     ::   a, b
+        integer                                    ::   x
+        !
+        real_is_equal_vect = .true.
+        do x = 1, 3
+            real_is_equal_vect =    real_is_equal_vect  .and.      abs(   a(x) - b(x)  )  < acc
+        end do
+        !
+        return
+    end function
+
+    logical function cplx_is_equal_vect(acc, a,b)
+        real(dp),                      intent(in)  ::   acc
+        complex(dp),   dimension(3),   intent(in)  ::   a, b
+        integer                                    ::   x
+        !
+        cplx_is_equal_vect = .true.
+        do x = 1, 3
+            cplx_is_equal_vect =    cplx_is_equal_vect  .and.      abs(   a(x) - b(x)  )  < acc
+        end do
+        !
+        return
+    end function
+!----------------------------------------------------------------------------------------------------------------------
+!
+!
+    subroutine real_tens_to_vect(tens, vect)
+        !   converts om_tens to a vector by applying Levi Cevita Tensor
+        !   see PRB 74, 195118 (2006)   Eq.(5)
+        real(dp),           intent(in)      ::  tens(:,:,:,:)
+        real(dp),           intent(out)     ::  vect(:,:,:)
+        integer                             ::  a, b, c
+        !
+        vect =  0.0_dp
+        do c = 1, 3
+            !
+            do b = 1, 3
+                do a = 1,3
+                    vect(c,:,:) = vect(c,:,:) + real(my_Levi_Civita(a,b,c),dp) * tens(a,b,:,:)
+                end do
+            end do
+            !
+        end do
+        return
+    end subroutine
+
+    subroutine cplx_tens_to_vect(tens, vect)
+        !   converts om_tens to a vector by applying Levi Cevita Tensor
+        !   see PRB 74, 195118 (2006)   Eq.(5)
+        complex(dp),        intent(in)      ::  tens(:,:,:,:)
+        complex(dp),        intent(out)     ::  vect(:,:,:)
+        integer                             ::  a, b, c
+        !
+        vect =   dcmplx(0.0_dp)
+        do c = 1, 3
+            !
+            do b = 1, 3
+                do a = 1,3
+                    vect(c,:,:) = vect(c,:,:) + real(my_Levi_Civita(a,b,c),dp) * tens(a,b,:,:)
+                end do
+            end do
+            !
+        end do
+        return
+    end subroutine
+!----------------------------------------------------------------------------------------------------------------------
+!
+!
+    subroutine real_matrix_comm(A, B, C)
         !   calculates the commutator of matrix A with matrix B
         !       C   = [A,B] =   AB - BA
+        real(dp),    intent(in)      ::  A(:,:),     B(:,:)
+        real(dp),    intent(out)     ::  C(:,:)
         !
+        C   = matmul(A, B)
+        C   = C - matmul(B, A)
+        !
+        return
+    end subroutine
+
+    subroutine cplx_matrix_comm(A,  B,  C)
+        !   calculates the commutator of matrix A with matrix B
+        !       C   = [A,B] =   AB - BA
         complex(dp),    intent(in)      ::  A(:,:),     B(:,:)
         complex(dp),    intent(out)     ::  C(:,:)
         !
@@ -136,91 +316,63 @@ module matrix_math
         !
         return
     end subroutine
+!----------------------------------------------------------------------------------------------------------------------
 
 
 
 
-!private:
 
-	subroutine errCheck(n,info, jobz)
-		!small subroutine used in subroutine eigSolver
-		implicit none
-		integer    , intent(in)        :: n,info
-		character(len=1), intent(in)   :: jobz
-		!
-		if(info > 0) then
-		      write(*,*) '[solver/errCheck]: WARNING, Problem solving the eigenvalue problem: '
-		      if(jobz .EQ. 'N') write(*,*) '[solver/errCheck]: the algorithm failed to converge; ', info ,&
-		      				' off-diagonal elements of an intermediate tridiagonal form did not converge to zero;'
-		      if(jobz .EQ. 'V') write(*,*) '[solver/errCheck]: the algorithm failed to compute an',&
-		    ' eigenvalue while working on the submatrix lying in rows and columns ', info/(n+1), ' through ', mod(info, n+1)
-		elseif(info < 0) then
-		     write(*,*) '[solver/errCheck]: the ',info,'-th parameter had an illegal value.' 
-		endif
-		!
-		return
+
+
+!private helpers
+    subroutine errCheck(n,info, jobz)
+        !small subroutine used in subroutine eigSolver
+        implicit none
+        integer    , intent(in)        :: n,info
+        character(len=1), intent(in)   :: jobz
+        !
+        if(info > 0) then
+              write(*,*) '[solver/errCheck]: WARNING, Problem solving the eigenvalue problem: '
+              if(jobz .EQ. 'N') write(*,*) '[solver/errCheck]: the algorithm failed to converge; ', info ,&
+                            ' off-diagonal elements of an intermediate tridiagonal form did not converge to zero;'
+              if(jobz .EQ. 'V') write(*,*) '[solver/errCheck]: the algorithm failed to compute an',&
+            ' eigenvalue while working on the submatrix lying in rows and columns ', info/(n+1), ' through ', mod(info, n+1)
+        elseif(info < 0) then
+             write(*,*) '[solver/errCheck]: the ',info,'-th parameter had an illegal value.' 
+        endif
+        !
+        return
     end subroutine
-
-
-!    subroutine normalCheck(n,eigVec)
-!    	!check if eigVec are orthonormal this should always be the case
-!		integer     , intent(in)      :: n
-!		complex(dp) , intent(in)      :: eigVec(:,:)
-!		integer                       :: i1,i2
-!		real(dp)                      :: tmp
-!		do i2=1,n
-!		      tmp = 0.0_dp
-!		      do i1=1,n
-!		            tmp = tmp + dconjg(eigVec(i1,i2))*eigVec(i1,i2)
-!		      end do
-!		      if(abs(tmp)-1.0_dp>1e-12_dp) then
-!		            write(*,*)"[solver/normalCheck]: WARNING eigVec not normal: ",dsqrt(tmp)
-!		      end if
-!		end do
-!		!
-!		return
-!	end subroutine
-
-
-    logical function is_equal_vect(acc, a,b)
-        real(dp),                   intent(in)     ::   acc
-        real(dp),   dimension(3),   intent(in)     ::   a, b
-        integer                                    ::   x
-        !
-        is_equal_vect = .true.
-        do x = 1, 3
-            is_equal_vect =    is_equal_vect  .and.      abs(   a(x) - b(x)  )  < acc
-        end do
-        !
-        return
-    end function
-
-    function crossPreal(a,b)
-        !cross product of two real 3dim vectors a,b
-        real(dp), dimension(3)              :: crossPreal
-        real(dp), dimension(3), intent(in)  :: a, b
-        !
-        crossPreal(1)   =   a(2) * b(3)     -   a(3) * b(2)  
-        crossPreal(2)   =   a(3) * b(1)     -   a(1) * b(3) 
-        crossPreal(3)   =   a(1) * b(2)     -   a(2) * b(1) 
-        !
-        return
-    end function
-
-
-    function crossPcplx(a,b)
-        !cross product of two complex 3dim vectors a,b
-        complex(dp),    intent(in)  :: a(3), b(3)
-        complex(dp)                 :: crossPcplx(3)
-        !
-        crossPcplx(1)   =   a(2) * b(3)     -   a(3) * b(2)  
-        crossPcplx(2)   =   a(3) * b(1)     -   a(1) * b(3) 
-        crossPcplx(3)   =   a(1) * b(2)     -   a(2) * b(1) 
-        !
-        return
-    end function
-
 
 
 
 end module
+
+    
+
+
+
+    
+
+
+    
+
+!    subroutine normalCheck(n,eigVec)
+!       !check if eigVec are orthonormal this should always be the case
+!       integer     , intent(in)      :: n
+!       complex(dp) , intent(in)      :: eigVec(:,:)
+!       integer                       :: i1,i2
+!       real(dp)                      :: tmp
+!       do i2=1,n
+!             tmp = 0.0_dp
+!             do i1=1,n
+!                   tmp = tmp + dconjg(eigVec(i1,i2))*eigVec(i1,i2)
+!             end do
+!             if(abs(tmp)-1.0_dp>1e-12_dp) then
+!                   write(*,*)"[solver/normalCheck]: WARNING eigVec not normal: ",dsqrt(tmp)
+!             end if
+!       end do
+!       !
+!       return
+!   end subroutine
+
