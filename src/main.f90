618806bbb3abe9c6d8a00b9eaea610720324a486
Compiler-max <max.merte@gmail.com>
2018-08-20 14:16:23 +0200

    cleaned up

--- src/main.f90
+++ src/main.f90
@@ -2,24 +2,22 @@ program MEPinterp
 	use mpi
 	use parameters,				only:		init_parameters,									&
 											mpi_root_id, mpi_id, mpi_nProcs, ierr
-	use mep_niu,				only:		mep_interp
+	use mep_niu,				only:		mep_worker
 	implicit none
-
-
+	!
+	!
 	!MPI INIT
 	mpi_root_id = 0
 	call MPI_INIT( ierr )
     call MPI_COMM_RANK (MPI_COMM_WORLD, 	mpi_id			, ierr)
     call MPI_COMM_SIZE (MPI_COMM_WORLD, 	mpi_nProcs		, ierr)
-
+    !
     !read the input file
     call init_parameters()
-
+    !
 	!do the interpolation
-	call mep_interp()			
-
-
+	call mep_worker()			
+	!
 	call MPI_FINALIZE(ierr)
 	stop
-
 end program 
