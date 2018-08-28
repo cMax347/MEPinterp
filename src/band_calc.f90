707f29880ba65963b55ebc99c322f418fb75d005
Compiler-max <max.merte@gmail.com>
2018-08-28 16:12:19 +0200

    own module for the bandstructure run, reads in k-mesh

---
+++ src/band_calc.f90
@@ -0,0 +1,64 @@
+module band_calc
+	use parameters,		only:			dp, mpi_root_id, mpi_id, mpi_nProcs, ierr,		&
+										seed_name
+	use file_io,		only:			read_kptsgen_pl_file,							&
+										mpi_read_tb_basis,								&
+										write_en_binary, 								&
+										write_en_global
+
+	implicit none
+
+
+	private
+	public		::						band_worker
+
+
+contains
+
+	subroutine band_worker()
+		real(dp),		allocatable			::	rel_kpts(:,:), en_k(:), R_vect(:,:)
+		integer								::	num_kpts, ki, k_per_mpi
+		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 			&
+												A_ka(:,:,:), Om_ka(:,:,:),				&
+												V_ka(:,:,:)
+
+
+
+		if( read_kptsgen_pl_file(rel_kpts)	) then
+			num_kpts	= size(rel_kpts,2)
+			k_per_mpi	= 0
+			!
+			!	get the data
+			call mpi_read_tb_basis(seed_name, R_vect, H_tb, r_tb)
+			!
+			allocate(	en_k(						size(H_tb,2)	)	)
+			allocate(	V_ka(	3,	size(H_tb,1),	size(H_tb,2)	)	)
+
+			!
+			!	do the work
+			do ki = mpi_id + 1, num_kpts,	mpi_nProcs
+				call get_wann_interp(H_tb, r_tb, R_vect, rel_kpts(:,ki), 	en_k, V_ka, A_ka, Om_ka )
+				call write_en_binary(ki,en_k)
+				k_per_mpi	= k_per_mpi + 1
+			end do
+			write(*,'(a,i10,a,i10,a)')	'[band_worker]:	interpolated ',k_per_mpi,' of ',num_kpts,' kpts'
+			!
+			!
+			!	write the results
+			call MPI_BARRIER(ierr)
+			if(mpi_id == mpi_root_id)	call write_en_global(rel_kpts)
+		else
+			stop 'for bandstructure calculations a kpts file has to be provided'
+		end if
+
+
+
+
+
+		return
+	end subroutine
+
+
+
+
+end module
