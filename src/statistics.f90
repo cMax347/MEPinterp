7ed96d20ebfac1221545cd29f4efc4498be5b636
Compiler-max <max.merte@gmail.com>
2018-09-13 15:38:59 +0200

    fermi dirac statistics

--- src/statistics.f90
+++ src/statistics.f90
@@ -1,5 +1,5 @@
 module statistics
-	use constants, 		only:			dp
+	use constants, 		only:			dp, kBoltz_Eh_K
 
 
 
@@ -18,11 +18,11 @@ contains
 	real(dp) pure function fermi_dirac(e_band, e_fermi,	T_kelvin) 
 		real(dp), 		intent(in)			::	e_band, e_fermi, T_kelvin
 		real(dp)							::	e_temp
-
-		e_temp	=	1.0
-
+		!
+		e_temp	=	kBoltz_Eh_K	*	T_kelvin
+		!
 		fermi_dirac = 1.0_dp	/	(		1.0_dp	+	exp(	(e_band	- e_fermi)	/	(e_temp))				)
-
+		!
 	end function
 
 
