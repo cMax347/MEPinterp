e044dab8992e0804acc22a4b08c33b670a368edf
Compiler-max <max.merte@gmail.com>
2018-09-13 15:38:38 +0200

    temperature

--- src/constants.f90
+++ src/constants.f90
@@ -17,11 +17,11 @@ module constants
 
 	!PHYSICAL
 	real(dp),		parameter 	::	aUtoAngstrm 	= 0.52917721092_dp, &
-									aUtoEv	 		= 27.211385_dp
-	real(dp),		parameter	::	aUtoTesla		= 235051.76_dp
+									aUtoEv	 		= 27.211385_dp							!Hartree to eV
+	real(dp),		parameter	::	aUtoTesla		= 235051.76_dp							!magn. field
 	real(dp),		parameter	::	speedOfLight	= 137.035999_dp !in atomic units
-	
-
+	real(dp),		parameter	::	kBoltz_Eh_K		= 8.6173303_dp * 1e-5_dp / aUtoEv 		!kBoltz from wiki: kB	=	8.6173303(50)×10−5	eV⋅K−1
+	real(dp),		parameter	::	T_room			= 300.0_dp
 
 
 end module constants
