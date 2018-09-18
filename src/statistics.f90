af3219b09672c7a987366e64e716ac10998af369
Compiler-max <max.merte@gmail.com>
2018-09-18 15:39:10 +0200

    removed some whitespaces

--- src/statistics.f90
+++ src/statistics.f90
@@ -1,27 +1,21 @@
 module statistics
 	use constants, 		only:			dp, kBoltz_Eh_K
 
-
-
 	implicit none
 
-
 	public				::	fermi_dirac
-
 	private
-
-
 
 contains
 
 
-	real(dp) pure function fermi_dirac(e_band, e_fermi,	T_kelvin) 
-		real(dp), 		intent(in)			::	e_band, e_fermi, T_kelvin
+	real(dp) pure function fermi_dirac(e_band, mu_chem,	T_kelvin) 
+		real(dp), 		intent(in)			::	e_band, mu_chem, T_kelvin
 		real(dp)							::	e_temp
 		!
 		e_temp	=	kBoltz_Eh_K	*	T_kelvin
 		!
-		fermi_dirac = 1.0_dp	/	(		1.0_dp	+	exp(	(e_band	- e_fermi)	/	(e_temp))				)
+		fermi_dirac = 1.0_dp	/	(		1.0_dp	+	exp(	(e_band	- mu_chem)	/	(e_temp))				)
 		!
 	end function
 
