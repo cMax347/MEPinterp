9403eea3fe7e2364006c33e069fa8f843f4d34ba
Compiler-max <max.merte@gmail.com>
2018-09-20 16:49:31 +0200

    FEATURE count electrons via fermi

--- src/statistics.f90
+++ src/statistics.f90
@@ -3,22 +3,37 @@ module statistics
 
 	implicit none
 
-	public				::	fermi_dirac
+	public				::	fd_stat, fd_get_N_el
 	private
 
 contains
 
 
-	real(dp) pure function fermi_dirac(e_band, mu_chem,	T_kelvin) 
-		real(dp), 		intent(in)			::	e_band, mu_chem, T_kelvin
-		real(dp)							::	e_temp
+	real(dp) pure function fd_stat(e_band, e_fermi,	T_kelvin) 
+		real(dp), 		intent(in)			::	e_band, e_fermi, T_kelvin
+		real(dp)							::	T_smear
 		!
-		e_temp	=	kBoltz_Eh_K	*	T_kelvin
+		T_smear			=	kBoltz_Eh_K		*	T_kelvin
 		!
-		fermi_dirac = 1.0_dp	/	(		1.0_dp	+	exp(	(e_band	- mu_chem)	/	(e_temp))				)
+		fd_stat		 	= 	1.0_dp	/	(		1.0_dp	+	exp(	(e_band	- e_fermi)	/	(T_smear))				)
 		!
+		return
 	end function
 
 
+
+
+	real(dp) pure function fd_get_N_el(en_k, e_fermi, T_kelvin)
+		real(dp),		intent(in)			::	en_k(:), e_fermi, T_kelvin
+		integer								::	n
+		!
+		fd_get_N_el	=	0.0_dp
+		!
+		do n = 1, size(en_k)
+			fd_get_N_el	=	fd_get_N_el	+	fd_stat(en_k(n),e_fermi, T_kelvin)
+		end do
+		!
+		return
+	end function	
 
 end module statistics
