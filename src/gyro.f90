5f83a08181b706ef59402a8a7dbec91d5bdf1a74
Compiler-max <max.merte@gmail.com>
2018-10-02 14:19:35 +0200

    new module for gytropic (souza) stuff

---
+++ src/gyro.f90
@@ -0,0 +1,35 @@
+module gyro
+	use constants,			only:		dp
+	use statistics,			only:		fd_stat_deriv
+
+
+
+	implicit none
+
+	public 	::	get_D
+	private
+
+
+	
+contains
+
+
+	subroutine get_D()
+		return
+	end subroutine
+
+
+	subroutine get_C()
+		return
+	end subroutine
+
+
+	subroutine get_K()
+		return
+	end subroutine
+
+
+
+
+
+end module gyro
