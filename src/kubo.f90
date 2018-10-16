3509b92943d4c5d79def5285eaac5d6c51f10219
Compiler-max <max.merte@gmail.com>
2018-10-16 11:54:50 +0200

    BUGFIX only calculate ahc if POS OP given

--- src/kubo.f90
+++ src/kubo.f90
@@ -28,15 +28,17 @@ contains
 		!	see wann guide chapter 12
 		!		eq. (12.16)
 		real(dp),		intent(in)		::	en_k(:), eFermi, T_kelvin, unit_vol
-		complex(dp),	intent(in)		::	Om_kab(:,:,:,:)
+		complex(dp), allocatable,	intent(in)		::	Om_kab(:,:,:,:)
 		real(dp)						::	o_ahc(3,3)
 		integer							::	n
 		!
 		o_ahc	=	0.0_dp
 		!
-		do n = 1, size(en_k)
-			o_ahc	=	o_ahc	-	real(Om_kab(:,:,n,n),dp)		*	fd_stat(en_k(n),	eFermi, 	T_kelvin) /	unit_vol
-		end do
+		if(allocated(Om_kab)) then
+			do n = 1, size(en_k)
+				o_ahc	=	o_ahc	-	real(Om_kab(:,:,n,n),dp)		*	fd_stat(en_k(n),	eFermi, 	T_kelvin) /	unit_vol
+			end do
+		end if
 		!
 		!
 		return
