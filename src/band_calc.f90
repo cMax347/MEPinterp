819061c781478f7b77b10f8d0ae0fd4faadf32aa
Compiler-max <max.merte@gmail.com>
2018-11-19 15:28:54 +0100

    now takes H_k from 3q-wrapper, diagonalizes and writes results

--- src/band_calc.f90
+++ src/band_calc.f90
@@ -8,13 +8,12 @@ module band_calc
 #endif
 	use constants,		only:			dp
 	use mpi_comm,		only:			mpi_root_id, mpi_id, mpi_nProcs, ierr
-	use input_paras,	only:			use_mpi, seed_name, a_latt
 	use k_space,		only:			get_recip_latt										
+	use wrapper_3q,		only:			get_ham
+	use matrix_math,	only:			zheevd_wrapper
 	use file_io,		only:			read_kptsgen_pl_file,							&
-										read_tb_basis,									&
 										write_en_binary, 								&
-										write_en_global
-	use wann_interp,	only:			get_wann_interp									
+										write_en_global							
 	implicit none
 
 
@@ -25,12 +24,10 @@ module band_calc
 contains
 
 	subroutine band_worker()
-		real(dp),		allocatable			::	rel_kpts(:,:), en_k(:), R_vect(:,:)
+		real(dp),		allocatable			::	rel_kpts(:,:), en_k(:)
 		real(dp)							::	recip_latt(3,3)
 		integer								::	num_kpts, ki, k_per_mpi
-		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 			&
-												A_ka(:,:,:), Om_kab(:,:,:,:),				&
-												V_ka(:,:,:)					!DO NOT ALLOCATE, ONLY THE POINTER IS NEEDED
+		complex(dp),	allocatable			::	H_k(:,:)
 		logical								::	do_gauge_trafo
 		!
 		if(mpi_id==mpi_root_id)	then
@@ -54,23 +51,23 @@ contains
 			num_kpts	= size(rel_kpts,2)
 			k_per_mpi	= 0
 			!
-			!	get the data
-			call read_tb_basis(seed_name, R_vect, H_tb, r_tb)
-			call band_allocator(H_tb, r_tb, en_k, A_ka, Om_kab)
+			!	allocate
+			num_wann	=	8
+			allocate(	en_k(		num_wann			)	)
+			allocate(	H_k(	num_wann,	num_wann	)	)
 			!
 			!
 			!	do the work
-			if(use_mpi)		call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+			if(mpi_nProcs>1)		call MPI_BARRIER(MPI_COMM_WORLD, ierr)
 			write(*,'(a,i3,a,a,a)')	'[#',mpi_id,';band_worker/',cTIME(time()),	']:	start interpolating...'
 			!
 			!
 			do_gauge_trafo	= .false. !eigenvalues are gauge independent
 			!
 			do ki = mpi_id + 1, num_kpts,	mpi_nProcs
-				call get_wann_interp(	do_gauge_trafo, H_tb, r_tb, 						&
-										a_latt, recip_latt, R_vect, ki, rel_kpts(:,ki),		& 
-										en_k, V_ka, A_ka, Om_kab							&
-									)
+				call get_ham(rel_kpts(:,ki),	H_k	)
+				call zheevd_wrapper(H_k, e_k)
+
 				call write_en_binary(ki,en_k)
 				k_per_mpi	= k_per_mpi + 1
 			end do
@@ -91,19 +88,5 @@ contains
 		return
 	end subroutine
 
-
-	subroutine band_allocator( H_tb, r_tb, en_k, A_ka, Om_kab)
-		real(dp),		allocatable			::	en_k(:)
-		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 			&
-												A_ka(:,:,:), Om_kab(:,:,:,:)									
-		!
-		allocate(	en_k(						size(H_tb,2)	)	)
-		if(	allocated(r_tb)	)	then	
-			allocate(	A_ka(	3,		size(r_tb,2),	size(r_tb,3)	)	)
-			allocate(	Om_kab(	3,3,	size(r_tb,2),	size(r_tb,3)	)	)
-		end if
-		!
-		return
-	end subroutine
 
 end module
