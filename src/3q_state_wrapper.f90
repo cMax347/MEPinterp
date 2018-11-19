7ed46677410e2ef2dabf8b12e1243c1aa385d6ab
Compiler-max <max.merte@gmail.com>
2018-11-19 14:13:15 +0100

    INIT wrapper for Jan's 3q_state code

---
+++ src/3q_state_wrapper.f90
@@ -0,0 +1,23 @@
+module 3q_state_wrapper
+	!	module for Wannier interpolation
+	!
+	!	the interpolation scheme from 
+	!			PRB 74, 195118 (2006) 
+	!	was used
+
+
+
+	implicit none
+
+
+	private
+	!public					::		get_wann_interp	
+
+
+	contains
+
+
+end module 3q_state_wrapper
+
+
+
