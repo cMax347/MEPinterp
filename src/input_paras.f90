04a64cf5544db521416c28bc4a2afb3c0d0189eb
Compiler-max <max.merte@gmail.com>
2018-11-19 15:29:33 +0100

    indicate the 3qstate int the init_parameter standard output

--- src/input_paras.f90
+++ src/input_paras.f90
@@ -141,7 +141,7 @@ module input_paras
 				!
 				!
 				write(*,*)					""
-				write(*,*)					"**********************init_parameter interpretation******************************"
+				write(*,*)					"**********************init_parameter interpretation (3q state mode most paras will be ignored)******************************"
 				write(*,*)					"parallelization with ",mpi_nProcs," MPI threads"
 				write(*,'(a,i3,a)')			"[#",mpi_id,";init_parameters]: input interpretation:"
 				write(*,*)					"[methods]"
