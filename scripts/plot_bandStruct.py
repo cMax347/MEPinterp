3ae7e2f0416d156cd6b42722845dce907e3639c0
Compiler-max <max.merte@gmail.com>
2018-12-12 11:10:58 +0100

    FEATURE indicate fermi level

--- scripts/plot_bandStruct.py
+++ scripts/plot_bandStruct.py
@@ -108,10 +108,12 @@ def plot_bandstruct(kpt_file, en_file, pdf_out_file, label_size=14, y_tick_size=
 	ax.set_xticklabels(k_labels,fontsize=label_size)
 	ax.grid(axis='x', alpha=.5, linewidth=.8,	color='black')
 
+	ax.set_yticks([0.],minor=True)
+	ax.grid(which='minor',axis='y')
 
 	#y-axis
-	ax.set_yticks([-9,-6,-3,0,3,6])
-	ax.set_ylim([-9.2,6.2])
+	ax.set_yticks([-12,-9,-6,-3,0,3,6])
+	#ax.set_ylim([-9.2,6.2])
 	plt.tick_params(axis='y', which='major',left=True,right=True, direction='in',labelsize=y_tick_size)
 	if plot_in_ev:
 		plt.ylabel(r'$E \,(eV)$',fontsize=label_size)
