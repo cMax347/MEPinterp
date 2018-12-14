2685fbc832a8c6ab1539a966c91ac252e53c441d
Compiler-max <max.merte@gmail.com>
2018-12-14 11:23:13 +0100

    FEATURE write velocities during bandstructure run

--- src/band_calc.f90
+++ src/band_calc.f90
@@ -8,12 +8,14 @@ module band_calc
 #endif
 	use constants,		only:			dp
 	use mpi_comm,		only:			mpi_root_id, mpi_id, mpi_nProcs, ierr
-	use input_paras,	only:			use_mpi, seed_name, a_latt
+	use input_paras,	only:			use_mpi, seed_name, a_latt, 					&
+										do_gauge_trafo, do_write_velo
 	use k_space,		only:			get_recip_latt										
 	use file_io,		only:			read_kptsgen_pl_file,							&
 										read_tb_basis,									&
 										write_en_binary, 								&
-										write_en_global
+										write_en_global,								&
+										write_velo
 	use wann_interp,	only:			get_wann_interp									
 	implicit none
 
@@ -28,10 +30,9 @@ contains
 		real(dp),		allocatable			::	rel_kpts(:,:), en_k(:), R_vect(:,:)
 		real(dp)							::	recip_latt(3,3)
 		integer								::	num_kpts, ki, k_per_mpi
-		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 			&
+		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 				&
 												A_ka(:,:,:), Om_kab(:,:,:,:),				&
-												V_ka(:,:,:)					!DO NOT ALLOCATE, ONLY THE POINTER IS NEEDED
-		logical								::	do_gauge_trafo
+												V_ka(:,:,:)					
 		!
 		if(mpi_id==mpi_root_id)	then
 			write(*,*)	"----------------------------------------------------------------"
@@ -56,7 +57,7 @@ contains
 			!
 			!	get the data
 			call read_tb_basis(seed_name, R_vect, H_tb, r_tb)
-			call band_allocator(H_tb, r_tb, en_k, A_ka, Om_kab)
+			call band_allocator(H_tb, r_tb, en_k, V_ka, A_ka, Om_kab)
 			!
 			!
 			!	do the work
@@ -64,14 +65,14 @@ contains
 			write(*,'(a,i3,a,a,a)')	'[#',mpi_id,';band_worker/',cTIME(time()),	']:	start interpolating...'
 			!
 			!
-			do_gauge_trafo	= .false. !eigenvalues are gauge independent
-			!
 			do ki = mpi_id + 1, num_kpts,	mpi_nProcs
 				call get_wann_interp(	do_gauge_trafo, H_tb, r_tb, 						&
 										a_latt, recip_latt, R_vect, ki, rel_kpts(:,ki),		& 
 										en_k, V_ka, A_ka, Om_kab							&
 									)
 				call write_en_binary(ki,en_k)
+				if(do_write_velo)	call write_velo(ki,V_ka)
+
 				k_per_mpi	= k_per_mpi + 1
 			end do
 			write(*,'(a,i3,a,a,a,i10,a,i10,a)')	'[#',mpi_id,';band_worker/',cTIME(time()),				&
@@ -92,12 +93,20 @@ contains
 	end subroutine
 
 
-	subroutine band_allocator( H_tb, r_tb, en_k, A_ka, Om_kab)
-		real(dp),		allocatable			::	en_k(:)
-		complex(dp),	allocatable			::	H_tb(:,:,:), r_tb(:,:,:,:), 			&
-												A_ka(:,:,:), Om_kab(:,:,:,:)									
+	subroutine band_allocator( H_tb, r_tb, en_k, V_ka, A_ka, Om_kab)
+		real(dp),		allocatable, intent(inout)			::	en_k(:)
+		complex(dp),	allocatable, intent(inout)			::	H_tb(:,:,:), r_tb(:,:,:,:), 	&
+																V_ka(:,:,:),					&
+																A_ka(:,:,:), Om_kab(:,:,:,:)									
 		!
-		allocate(	en_k(						size(H_tb,2)	)	)
+		allocate(	en_k(size(H_tb,2))		)
+		!
+		if( do_write_velo )		then
+			allocate(	V_ka(3,	size(H_tb,1), size(H_tb,2))		)
+			write(*,'(a,i3,a,i1,a,i3,a,i3,a)')	"[",mpi_id,"]V_ka allocated with size=	(",size(V_ka,1), "x",size(V_ka,2),"x",size(V_ka,3),")"
+		end if
+		!
+		!
 		if(	allocated(r_tb)	)	then	
 			allocate(	A_ka(	3,		size(r_tb,2),	size(r_tb,3)	)	)
 			allocate(	Om_kab(	3,3,	size(r_tb,2),	size(r_tb,3)	)	)
