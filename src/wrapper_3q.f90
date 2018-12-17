9833fc40235d0bc3f13793b2bb8accadc79ec2cd
Compiler-max <max.merte@gmail.com>
2018-12-17 17:42:51 +0100

    no additional output from this module in debug mode

--- src/wrapper_3q.f90
+++ src/wrapper_3q.f90
@@ -2,7 +2,6 @@ module wrapper_3q
 	use constants,			only:		sp, dp, aUtoEv
 	use mpi_comm,			only:		mpi_id
 	use m_setham_FeMn,		only:		read_inp,	init_ham
-	use input_paras,		only:		debug_mode
 
 	implicit none
 
@@ -72,44 +71,6 @@ module wrapper_3q
 		V_dp(	1	,:,:)	=	real(	vx_sp(:,:)	,dp)	/	aUtoEv
 		V_dp(	2	,:,:)	=	real(	vy_sp(:,:)	,dp)	/	aUtoEv
 		V_dp(	3	,:,:)	=	real(	vz_sp(:,:)	,dp)	/	aUtoEv
-		!
-		!
-		!
-		!
-		!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^				
-		!			PRINT HAM																 |
-		!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		!
-		!	sp
-		if(debug_mode)	then
-			write(*,'(a,i3,a)')	'[#',mpi_id,';	get_ham]	finished hamiltonian setup H_sp:'
-			do row = 1, num_wann
-				write(*,"(100(a,f5.2,a,f5.2,a))")	(	"(",real(H_sp(row,clm)),"+i* ",imag(H_sp(row,clm)),")	"	,	clm = 1, num_wann		)
-			end do
-			!
-			write(*,'(a,i3,a)')	'[#',mpi_id,';	get_ham]	got velocities:'
-			write(*,*)	"	n	| 	m 	||		vx 			|			vy 		|			vz				"
-			write(*,*)	"-------------------------------------------------------------------------------------------------"
-			do m =	1, size(V_dp,3)
-				do n = 1, size(V_dp,2)
-				!	
-				write(*,'(a,i4,a,i4,a)',advance="no")			" ",n," | ",m, "	||	"
-				write(*,'(a,f7.3,a,f7.3,a)',advance="no")		" (",dreal(	V_dp(1,n,m)	),"+i*",aimag( V_dp(1,n,m)),")	| "
-				write(*,'(a,f7.3,a,f7.3,a)',advance="no")		" (",dreal(	V_dp(2,n,m)	),"+i*",aimag( V_dp(2,n,m)),")	| "
-				write(*,'(a,f7.3,a,f7.3,a)')					" (",dreal(	V_dp(3,n,m)	),"+i*",aimag( V_dp(3,n,m)),")	| "
-
-				!
-				end do
-				write(*,*)	"------"
-			end do
-			write(*,*)	"-------------------------------------------------------------------------------------------------"
-		end if
-		!
-		!	dp
-		!write(*,'(a,i3,a)')	'[#',mpi_id,';	get_ham]	finished hamiltonian setup H_dp:'
-		!do row = 1, num_wann
-		!	write(*,"(100(a,f5.2,a,f5.2,a))")	(	"(",dreal(H_dp(row,clm)),"+i* ",aimag(H_dp(row,clm)),")	"	,	clm = 1, num_wann		)
-		!end do
 		!
 		!
 	end subroutine
