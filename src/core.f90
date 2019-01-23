8aaec2d12eb63d894fb8999a7f4b098b9f48d6b7
Compiler-max <max.merte@gmail.com>
2019-01-23 17:18:57 +0100

    FEATURE no connection calculations when connection not present

--- src/core.f90
+++ src/core.f90
@@ -244,9 +244,11 @@ contains
 								!	OPTICAL CONDUCTIVITY (via conn)	& 	2nd order photo conductivity
 								!----------------------------------------------------------------------------------------------------------------------------------
 								if(		do_opt	)														then
-									call kubo_opt_tens(hw_tmp, eF_tmp, T_kelvin, i_eta_smr, en_k, A_ka, 		tempS, tempA)
-									kubo_opt_s_loc(:,:,hw_idx,eF_idx)	=	kubo_opt_s_loc(:,:,hw_idx,eF_idx)	+	tempS							
-									kubo_opt_a_loc(:,:,hw_idx,eF_idx)	=	kubo_opt_a_loc(:,:,hw_idx,eF_idx)	+	tempA
+									if allocated(A_ka) then
+										call kubo_opt_tens(hw_tmp, eF_tmp, T_kelvin, i_eta_smr, en_k, A_ka, 		tempS, tempA)
+										kubo_opt_s_loc(:,:,hw_idx,eF_idx)	=	kubo_opt_s_loc(:,:,hw_idx,eF_idx)	+	tempS							
+										kubo_opt_a_loc(:,:,hw_idx,eF_idx)	=	kubo_opt_a_loc(:,:,hw_idx,eF_idx)	+	tempA
+									end if
 									!
 									photo2_cond_loc(:,:,hw_idx,eF_idx)	=	photo2_cond_loc(:,:,hw_idx,eF_idx)	+	photo_2nd_cond(hw_tmp, phi_laser, eF_tmp, &
 								 																			T_kelvin, i_eta_smr, en_k, V_ka)
