90ca65ac0b01b995e1c4eb091fd46e2241917e0a
Compiler-max <max.merte@gmail.com>
2019-02-15 14:47:15 +0100

    BUGFIX script can handle the new out folder style now

--- scripts/plot_delta_hw.py
+++ scripts/plot_delta_hw.py
@@ -16,12 +16,24 @@ from fortran_io			import 	read_cmplx_tens_file
 
 class HW_probe:
 
-	def __init__(self, root_dir):
+	def __init__(self, root_dir,dir_id):
 		#derived attributes
 		self.root_dir	= root_dir
 		self.plot_dir	= self.root_dir+'/plots'
+		self.dir_id		= dir_id
 		self.n_bands	= 0
-
+		#
+		#
+		dir_list		= os.listdir(self.root_dir)
+		self.subdirs  	= []
+		self.para_vals	= []
+		for elem in dir_list:
+			path = self.root_dir+'/'+elem
+			if os.path.isdir(path):
+				if (self.dir_id in elem):
+					self.subdirs.append(path)
+					self.para_vals.append(	elem.split(dir_id) [1]	)
+		#
 		#
 		#	hw list & energies
 		self.hw_lst				=	[]
@@ -47,7 +59,9 @@ class HW_probe:
 		print("~")
 		print("[init]: will search for data in folder: "	+	self.root_dir	)
 		print("[init]: will output to folder: "				+ 	self.plot_dir	)
-		
+		print("[init]: found data folders:")
+		for idx, sub in enumerate(self.subdirs):
+			print('[init]:		',sub, " intepreted as ",dir_id,'=',self.para_vals[idx])
 	
 
 	def __del__(self):
@@ -86,7 +100,7 @@ class HW_probe:
 		return 		ahc_tens, ahc_kubo_tens, ohc_kubo_tens, SeCnd_opt_tens, optA_tens, optS_tens
 
 
-	def save_attach_subData(		self, work_dir, hw,
+	def save_attach_subData(		self, work_dir,para_val, hw,
 									ahc_tens, ahc_kubo_tens, ohc_kubo_tens,	
 									SeCnd_opt_tens, optA_tens, optS_tens
 							):
@@ -97,32 +111,33 @@ class HW_probe:
 		#--------------------------
 		#	HALL LIKE
 		#
+		#
 		if len(ahc_tens)	is 3:
-			self.hw_ahc_data.append(				[hw, ahc_tens]			)
+			self.hw_ahc_data[-1].append(				[hw, ahc_tens]			)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong ahc length in "+					str(work_dir)		)
 			print("[save_attach_subData]: ahc_tens:"+str(ahc_tens))
 		if len(ahc_kubo_tens) is 3:
-			self.hw_ahc_kubo_data.append(			[hw, ahc_kubo_tens]		)
+			self.hw_ahc_kubo_data[-1].append(			[para_val, hw, ahc_kubo_tens]		)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong ahc kubo length in "+			str(work_dir)		)
 		if len(ohc_kubo_tens) is 3:
-			self.hw_ohc_data.append(				[hw, ohc_kubo_tens]		)
+			self.hw_ohc_data[-1].append(				[para_val, hw, ohc_kubo_tens]		)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong ohc kubo length in "+			str(work_dir)		)
 		#--------------------------
 		#	OPTICAL
 		#
 		if len(SeCnd_opt_tens)	is 3:
-			self.hw_2ndPhoto_data.append(			[hw,	SeCnd_opt_tens]		)
+			self.hw_2ndPhoto_data[-1].append(			[para_val, hw,	SeCnd_opt_tens]		)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong 2nd photo length in "+			str(work_dir)		)
 		if len(optA_tens)	is 3:
-			self.hw_optA_data.append(			[hw,	optA_tens]		)
+			self.hw_optA_data[-1].append(			[para_val, hw,	optA_tens]		)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong opt asymm tens length in "+		str(work_dir)		)
 		if len(optS_tens)	is 3:
-			self.hw_optS_data.append(			[hw,	optS_tens]		)
+			self.hw_optS_data[-1].append(			[para_val, hw,	optS_tens]		)
 		else:
 			print("[save_attach_subData]: 	WARNING  wrong opt ssymm tens length in "+		str(work_dir)		)
 
@@ -131,88 +146,72 @@ class HW_probe:
 
 
 	def read_hw_lst(self,out_dir):
-		self.hw_lst	=	np.genfromtxt(out_dir+'/hw_lst.txt', skip_header=1,usecols=(1))
+		this_hw_lst				=	np.genfromtxt(out_dir+'/hw_lst.txt', skip_header=1,usecols=(1))
+		self.hw_lst.append(	this_hw_lst	)
 
 
 	def read_ahc_lst(self,ahc_dir):
 		self.hw_ahc_data.append(	)
 
 
-	def read_hw(self):
+	def read_data(self):
 		print("^")
 		print("^")
 		print("		TRAVERSE SUBDIRECOTRIES - COLLECT THE DATA	")
 		print("-------------------------------------------------------------------------------")
 		
-
-		out_dir		=	self.root_dir+'/out'
-		ahc_dir		=	out_dir+'/ahc'
-
-		# READ HW LIST
-		self.read_hw_lst(out_dir)
-
-
-		#	loop hw_idx
-		for hw_idx, hw_val in enumerate(self.hw_lst):
-			print('[read_hw]: now try to read #hw ',hw_idx,"= ",hw_val," (eV)")
-			ahc_tens, ahc_kubo_tens, ohc_kubo_tens, SeCnd_opt_tens, optA_tens, optS_tens	=	self.read_responses(out_dir,hw_idx)
-			#
-			self.save_attach_subData(	out_dir, hw_val, 	
-										ahc_tens, ahc_kubo_tens, ohc_kubo_tens, 
-										SeCnd_opt_tens, optA_tens, optS_tens
-									)
-
-
-
-		#	plot hall like neeeds
-		# self.hw_lst:
-		# self.hw_ahc_data:
-		# self.hw_ahc_kubo_data:
-		# self.hw_ohc_data:
-
-
-		##	LOOP DIRECTORIES
-		#for entry in os.scandir(self.root_dir):
-		#	if entry.is_dir and 'plots' not in entry.path:
-		#		#
-		#		#
-		#		work_dir	=	entry.path
-		#		hw 		=	float(work_dir.split("hw")[1])	
-		#		#
-		#		#	READ DATA FROM NEW SUBDIR
-		#		print("[read_hw]: found subdir ="+str(entry.path)+' assoc. hw='+str(hw))
-		#		mep_tens, mep_cs, mep_lc, mep_ic,	ahc_tens, ahc_kubo_tens, ohc_kubo_tens,	SeCnd_opt_tens, optA_tens, optS_tens	=	self.read_subdirectory(work_dir)
-		#		#
-		#		self.save_attach_subData( work_dir,	hw,ahc_tens, ahc_kubo_tens, ohc_kubo_tens,	SeCnd_opt_tens, optA_tens, optS_tens	)
-		#		#
-		#		print("----------------------------------------------------------------------")
-		#		print("")
-		##
-		##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-		## ~					 ~
-		##	SORT BY HW VALUE
-		## ~					 ~
-		##--
-		#self.hw_lst				=	sorted( self.hw_lst						)
+		#if self.dir_id in curr_dir:
+		#	delta  		=	curr_dir.split(self.dir_id) [1]
+		#	print('found new subdir with delta = ', delta)
+		#	#
+		#	out_dir		=	curr_dir+'/out'
+		#	ahc_dir		=	out_dir+'/ahc'
+		#	#
+		#	# READ HW LIST
+		#	self.read_hw_lst(out_dir)
 		
-		##
-		#self.hw_cs_data			=	sorted(	self.hw_cs_data					)
-		#self.hw_lc_data			=	sorted(	self.hw_lc_data					)
-		#self.hw_ic_data			=	sorted(	self.hw_ic_data					)
-		##
-		#self.hw_ahc_data		=	sorted(	self.hw_ahc_data				)
-		#self.hw_ahc_kubo_data	=	sorted(	self.hw_ahc_kubo_data			)
-		#self.hw_ohc_data		=	sorted(	self.hw_ohc_data				)
-		##
-		#self.hw_2ndPhoto_data	=	sorted(	self.hw_2ndPhoto_data			)
-		#self.hw_optA_data		=	sorted(	self.hw_optA_data				)
-		#self.hw_optS_data		=	sorted(	self.hw_optS_data				)
+
+
+		#	get data from all subdirs
+		for idx, path in enumerate(self.subdirs):
+			out_dir	=	path + '/out'
+			#
+			# add container for this data set
+			self.hw_ahc_data.append(		[])
+			self.hw_ahc_kubo_data.append(	[])
+			self.hw_ohc_data.append(		[])
+			self.hw_2ndPhoto_data.append(	[])
+			self.hw_optA_data.append(		[])
+			self.hw_optS_data.append(		[])
+			#
+			#	get hw lst of out_dir
+			self.read_hw_lst(out_dir)
+			#	loop hw_idx
+			#
+			#	make a plot for this hw_lst 
+			for hw_idx, hw_val in enumerate(self.hw_lst[-1]):
+				swag = 1
+				#print('[read_hw]: now try to read #hw ',hw_idx,"= ",hw_val," (eV)")
+				ahc_tens, ahc_kubo_tens, ohc_kubo_tens, SeCnd_opt_tens, optA_tens, optS_tens	=	self.read_responses(out_dir,hw_idx)
+				#
+				self.save_attach_subData(	out_dir, self.para_vals[idx], hw_val, 	
+											ahc_tens, ahc_kubo_tens, ohc_kubo_tens, 
+											SeCnd_opt_tens, optA_tens, optS_tens
+										)
+			#
+			print('[read_data]: read data from ',out_dir, ' (associated ',self.dir_id,'=',self.para_vals[idx],')')
+
+	
+		zipped	= zip(		self.para_vals, 
+						self.hw_ahc_data, self.hw_ahc_kubo_data, self.hw_ohc_data, 
+						self.hw_2ndPhoto_data, 
+						self.hw_optA_data, self.hw_optS_data
+					)
+		sort	=	sorted(zipped)
+		self.para_vals, self.hw_ahc_data, self.hw_ahc_kubo_data, self.hw_ohc_data, self.hw_2ndPhoto_data, self.hw_optA_data, self.hw_optS_data= map(list,zip(*sort))
+
 		##~~~~~~~~~~~~~~~~~~~~~~~~
-		print("[read_hw]: hw list = ",self.hw_lst)
-
-
-
-
+		
 
 
 
@@ -221,19 +220,22 @@ class HW_probe:
 		unit_dsc	=	"atomic units"
 		unit_str	=	r'$e^2$/ ($\hbar a_0$)'
 		#
+		au_to_S 		=	2.4341348e-4	#3.874046 * 1e-5			#	(e**2/h)	-> (S) Siemens
+		au_to_cm		=	5.2917721e-9	# * 1e-9
+
+
+		au_to_S_cm		=	au_to_S	/ au_to_cm
+		#
 		if units == "scale":
 			unit_dsc	=	"use the scale given as function argument (sort of a wildcard)"
 			unit_str	=	"-"
 		elif units == "SI":
-			au_to_S_cm		=	4.599848 * 10**4
-			scale		=	au_to_S_cm
+			scale			=	scale * au_to_S_cm
 			unit_str		=	"[S/cm]"
 			unit_dsc		=	"SI units"	
-		elif units == "wx":
-			au_to_S_cm		=	4.599848 * 10**4
-		 	
-			scale			=	au_to_S_cm	/ 100.
-			unit_str		=	"[arb. u.]"
+		elif units == "wx":		 	
+			scale			=	scale *au_to_S_cm	/ 100.
+			unit_str		=	r'[$10^2$ S/cm]'
 			unit_dsc		=	"Units used by wanxiang in his paper. this should be the SI value divided by 100"	
 		#
 		print('[set_hall_units]:  chooen units "'+units+'" with dim '+unit_str+'" and  descriptor: "'+unit_dsc+'" '	)
@@ -242,11 +244,13 @@ class HW_probe:
 
 
 
+
+
 			
 
 	def plot_hall_like(		self, units='au', scale=1.0, 
 							plot_ahc=True, plot_ahc_kubo= True, plot_ohc=True, 
-							label_size=14, xtick_size=12, ytick_size=12,
+							line_width=1, label_size=14, xtick_size=12, ytick_size=12,
 							marker_size=12,
 							re_bound=1, im_bound=1
 					):
@@ -275,19 +279,34 @@ class HW_probe:
 		ohc_kubo_data	= []
 		#
 
+		para_floats	=	[]
+		for para_str in self.para_vals:
+			para_floats.append(float(para_str))
+
+		#print(self.hw_ahc_kubo_data)
+
 		scale, unit_str, unit_dsc	=	self.set_hall_units(units,scale)
 
-		for hw in self.hw_lst:
-			hw_plot.append(hw)
-		for ahc_tens in self.hw_ahc_data:
-			ahc_data.append(						ahc_tens[1]				)
-		for ahc_kubo_tens in self.hw_ahc_kubo_data:
-			ahc_kubo_data.append(					ahc_kubo_tens[1]		)
-		for ohc_tens in self.hw_ohc_data:
-			ohc_kubo_data.append(					ohc_tens[1]				)
+
+		for idx,para_val in enumerate(self.para_vals):
+			hw_plot.append([])
+			ahc_data.append([])
+			ahc_kubo_data.append([])
+			ohc_kubo_data.append([])
+			#
+			for hw in self.hw_lst[idx]:
+				hw_plot[-1].append(hw)
+			for ahc_tens in self.hw_ahc_data[idx]:
+				ahc_data[-1].append(						ahc_tens[1]				)
+			for ahc_kubo_tens in self.hw_ahc_kubo_data[idx]:
+				ahc_kubo_data[-1].append(					ahc_kubo_tens[2]		)
+			for ohc_tens in self.hw_ohc_data[idx]:
+				ohc_kubo_data[-1].append(					ohc_tens[2]				)
+
+		#print(ahc_kubo_data)
 		#
 		#
-		
+	
 		#
 		#
 		dim_str	= []
@@ -295,78 +314,138 @@ class HW_probe:
 		dim_str.append('y')
 		dim_str.append('z')
 
-		#	uncomment for debugging purposes
+		##	uncomment for debugging purposes
 		#print("[plot_hall_like]: hw list: 	", self.hw_tot_data	)
 		#print("[plot_hall_like]: ahc list: 	", self.hw_ahc_data)
 		#print("[plot_hall_like]: ahc kubo  list: 	", self.hw_ahc_kubo_data)
 		#print("[plot_hall_like]: ohc kubo  list: 	", self.hw_ohc_data)
-		hw_max	=	max(self.hw_lst)
-		hw_min	=	min(self.hw_lst)
+		#hw_max	=	max(self.hw_lst)
+		#hw_min	=	min(self.hw_lst)
 
 
 		#	color code for the AHC plot
-		ahc_col		=	'black'
-		ohc_wx_col	=	'blue'
-		ohc_w90_col	=	'orange'	
+		zero_col=	'black'
+		neg_col	=	'orange'
+		pos_col	=	'blue'
 
 		#
 		#plot all 9 components of mep tensor
 		for i in range(0,3):
 			for j in range(0,3):
 				#COLLECT a_ij(phi=0:n_phi)
-				ahc_plot		 	= []
-				RE_ahc_kubo_plot	= []
-				IM_ahc_kubo_plot	= []
-				RE_ohc_kubo_plot	= []
-				IM_ohc_kubo_plot	= []
-				#				
-				for ahc_tens in ahc_data:
-					ahc_plot.append(						scale * np.real(	ahc_tens[i][j]		)				)
-				for ahc_K_tens in ahc_kubo_data:
-					RE_ahc_kubo_plot.append(				scale * np.real(	ahc_K_tens[i][j]	)				)
-					IM_ahc_kubo_plot.append(				scale * np.imag(	ahc_K_tens[i][j]	)				)
-				for ohc_tens in ohc_kubo_data:
-					RE_ohc_kubo_plot.append(				scale * np.real(	ohc_tens[i][j]		)				)
-					IM_ohc_kubo_plot.append(				scale * np.imag(	ohc_tens[i][j]		)				)
-				
-
-				#
-				#
 				#do PLOT
 				fig, ax  = plt.subplots(2,1, sharex=True)
-				ax[0].set_title('optical Hall conductivity')
-				#
-				#		REAL PART
-				#
+				
+
+				title = ''
 				if plot_ahc:
-					try:
-						ax[0].plot(hw_plot, ahc_plot,'-', color=ahc_col,label=r'$ \: \: \:\: \;  \sigma ^{\mathrm{AHC}}$')
-					except:
-						print("[plot_hall_like]: 	WARNING could not plot AHC tensor")
+					title = title + ' AHC '
 				if plot_ahc_kubo:
-					try:
-						ax[0].plot(hw_plot, RE_ahc_kubo_plot,		'v-', 	color=ohc_wx_col, markersize=marker_size,		label=r'Wx'		)
-					except:
-						print("[plot_hall_like]: 	WARNING could not plot RE AHC_Kubo (wann guide) tensor")
+					title = title + ' OHC-Wx'
 				if plot_ohc:
-					try:
-						ax[0].plot(hw_plot, RE_ohc_kubo_plot,		'*-', 	color=ohc_w90_col, markersize=marker_size,		label=r'w90'		)
-					except:
-						print("[plot_hall_like]: 	WARNING could not plot RE AHC_Kubo (wanxiang) tensor")
+					title = title + ' OHC-w90'
+				ax[0].set_title(title)
 				#
-				#
-				#		IMAGINARY PART
-				#
-				if plot_ahc_kubo:
-					try:
-						ax[1].plot(hw_plot, IM_ahc_kubo_plot,		'v-', 	color=ohc_wx_col, markersize=marker_size,		label=r'Wx'		)
-					except:
-						print("[plot_hall_like]: 	WARNING could not plot IM AHC_Kubo (wann guide) tensor")
-				if plot_ohc:
-					try:
-						ax[1].plot(hw_plot, IM_ohc_kubo_plot,		'*-', 	color=ohc_w90_col, markersize=marker_size,		label=r'w90'		)
-					except:
-						print("[plot_hall_like]: 	WARNING could not plot IM OHC_Kubo (wanxiang) tensor")
+				for idx, para_val in enumerate(self.para_vals):	
+					print('[plot_hall_like]: start with (',dim_str[i],',',dim_str[j],')',self.dir_id,'=',para_val)				
+					ahc_plot		 	= []
+					RE_ahc_kubo_plot	= []
+					IM_ahc_kubo_plot	= []
+					RE_ohc_kubo_plot	= []
+					IM_ohc_kubo_plot	= []
+					#				
+					for ahc_tens in ahc_data[idx]:
+						ahc_plot.append(						scale * np.real(	ahc_tens[i][j]		)				)
+					for ahc_K_tens in ahc_kubo_data[idx]:
+						RE_ahc_kubo_plot.append(				scale * np.real(	ahc_K_tens[i][j]	)				)
+						IM_ahc_kubo_plot.append(				scale * np.imag(	ahc_K_tens[i][j]	)				)
+					for ohc_tens in ohc_kubo_data[idx]:
+						RE_ohc_kubo_plot.append(				scale * np.real(	ohc_tens[i][j]		)				)
+						IM_ohc_kubo_plot.append(				scale * np.imag(	ohc_tens[i][j]		)				)
+					#
+					hw_max	=	max(hw_plot[idx])
+					hw_min	=	min(hw_plot[idx])
+					#
+					#
+					#	determine color based on para_val
+					para_val = float(para_val)
+					if abs( para_val-1.0 ) < 0.01:
+						color	=	zero_col 
+					elif para_val < 1.0:
+						color	=	neg_col
+					else:
+						color	=	pos_col
+					
+					
+
+					#	set the midrange curves to be dotted
+					style='-'
+					if (para_val > min(para_floats)) and (para_val < max(para_floats)):
+						if( abs(para_val-1.0) > 0.01):
+							style='-.' 
+
+
+					label =r'$\delta$ '+'{:.2f}'.format(float(para_val))
+					#		REAL PART
+					#
+					if plot_ahc:
+						#print('hw_plot=',hw_plot)
+						#print('ahc_plot=',ahc_plot)
+						try:
+							ax[0].plot(hw_plot[0], ahc_plot,'-', color=color,label=label)
+						except:
+							print("[plot_hall_like]: 	WARNING could not plot AHC tensor")
+					if plot_ahc_kubo:
+						try:
+							ax[0].plot(	hw_plot[0], RE_ahc_kubo_plot,		
+													marker='v',
+													linestyle=style,
+													linewidth=line_width, 	
+													color=color ,	 
+													markersize=marker_size,		
+													label=label		
+										)
+						except:
+							print("[plot_hall_like]: 	WARNING could not plot RE AHC_Kubo (wann guide) tensor")
+					if plot_ohc:
+						try:
+							ax[0].plot(hw_plot[0], RE_ohc_kubo_plot,		
+														marker='*', 	
+														linestyle=style,
+														linewidth=line_width, 	
+														color=color, 
+														markersize=marker_size,		
+														label=label		)
+						except:
+							print("[plot_hall_like]: 	WARNING could not plot RE AHC_Kubo (wanxiang) tensor")
+					#
+					#
+					#		IMAGINARY PART
+					#
+					if plot_ahc_kubo:
+						try:
+							ax[1].plot(hw_plot[0], IM_ahc_kubo_plot,		
+													marker='v',
+													linestyle=style,
+													linewidth=line_width, 	 	
+													color=color, 
+													markersize=marker_size,		
+													label=label	
+										)
+						except:
+							print("[plot_hall_like]: 	WARNING could not plot IM AHC_Kubo (wann guide) tensor")
+					if plot_ohc:
+						try:
+							ax[1].plot(hw_plot[0], IM_ohc_kubo_plot,		
+														marker='*',
+														linestyle=style,
+														linewidth=line_width, 	 	
+														color=color, 
+														markersize=marker_size,		
+														label=label	)
+						except:
+							print("[plot_hall_like]: 	WARNING could not plot IM OHC_Kubo (wanxiang) tensor")
+				# ~~~~~~~~~~~~~~~~~
 				#
 				#
 				#	LABELS & TITLES
@@ -388,12 +467,12 @@ class HW_probe:
 				#
 				#
 				#ax.set_ylim([mep_min,mep_max])
-				ax[0].tick_params(axis='y',which='major', direction='in',labelsize=ytick_size)
-				ax[0].tick_params(axis='x',which='both', direction='in',labelsize=xtick_size)
-				ax[1].tick_params(axis='x',which='both', direction='in',labelsize=xtick_size, top=True)
-				ax[1].tick_params(axis='y',which='major', direction='in',labelsize=ytick_size)
+				#ax[0].tick_params(axis='y',which='major', direction='in',labelsize=ytick_size)
+				#ax[0].tick_params(axis='x',which='both', direction='in',labelsize=xtick_size)
+				#ax[1].tick_params(axis='x',which='both', direction='in',labelsize=xtick_size, top=True)
+				#ax[1].tick_params(axis='y',which='major', direction='in',labelsize=ytick_size)
 				#
-				plt.legend()
+				plt.legend(loc='lower right')
 				plt.tight_layout()
 				fig.subplots_adjust(hspace=0.01)
 				#
@@ -436,11 +515,11 @@ class HW_probe:
 
 
 
-def plot_hw(root_dir):
+def plot_delta(root_dir):
 	#	use the above class in here to plot data in folder root_dir
 	print('[plot_hw]:	hello there')
 
-	dir_id	=	"hw"
+	dir_id	=	"delta"
 
 
 
@@ -449,13 +528,15 @@ def plot_hw(root_dir):
 		#
 		# loop curr dir
 		#	split dir by dir_id
-		hw_val =	curr_dir.split(dir_id)[1]
+		#d_val =	curr_dir.split(dir_id)[1]
 		#
 		#myTest	= HW_probe(curr_dir)
-		#myTest	= HW_probe(root_dir)
-		#print('[plot_hw]:	initalized plotting of '+str(root_dir))
+
+		myTest	= HW_probe(root_dir,dir_id)
+		print('[plot_hw]:	initalized plotting of '+str(root_dir))
 		#
-		myTest.read_hw()
+
+		myTest.read_data()
 		print('[plot_hw]:	read folder, start plotting....')
 		print("..")
 		print("..")
@@ -468,18 +549,18 @@ def plot_hw(root_dir):
 		#
 		myTest.plot_hall_like(		units			=		'wx'		, 
 									scale			=		1.0			, 
-									plot_ahc		=		True		, 
+									plot_ahc		=		False		, 
 									plot_ahc_kubo	= 		True		, 
-									plot_ohc		=		True		, 
-									label_size=14, xtick_size=12, ytick_size=12, marker_size=4,
-									re_bound		=	35,
-									im_bound		=	10
+									plot_ohc		=		False		, 
+									line_width=1.5,label_size=14, xtick_size=12, ytick_size=12, marker_size=1.1,
+									re_bound		=	2.5,
+									im_bound		=	5
 							)
 		print("...")
 		print('[plot_hw]:	plotted Hall like tensors')
 		#	~~~~~~~~~~~~~~~~~~~~~~~~
 		#
-		myTest.plot_opt()
+		#myTest.plot_opt()
 		print("...")
 		print('[plot_hw]:	plotted optical cond. tensors')
 		#----------------------------------------------------------------------------------------------------------------------
@@ -499,7 +580,7 @@ if len(sys.argv) <	2:
 	print("please pass a folder to plot")
 else:
 	root_dir	=	sys.argv[1]
-	plot_hw(root_dir)
+	plot_delta(root_dir)
 
 
 
