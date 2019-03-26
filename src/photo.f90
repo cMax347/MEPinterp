6c636fc7d7b0d145ecf7856fd41104dca6a35872
Compiler-max <max.merte@gmail.com>
2019-03-26 11:26:40 +0100

    FEATURE write directly to target

--- src/photo.f90
+++ src/photo.f90
@@ -22,23 +22,21 @@ contains
 		!
 		real(dp),		intent(in)			::	hw_lst(:), fd_distrib(:,:), en_k(:)
 		complex(dp),	intent(in)			::	V_ka(:,:,:), phi_laser, i_eta_smr
-		real(dp),		allocatable			::	phot_cond(:,:,:,:,:), tmp(:,:,:,:), df_ln(:)
+		real(dp),		allocatable			::	phot_cond(:,:,:,:,:), df_ln(:)
 		complex(dp)							::	dE_nm, dE_nl,  dE_nl_hw, vvv_nl_lm_mn(3,3,3)
 		integer								::	m, n, l, c, b, hw, omega, ef_idx, n_ef, n_hw, n_wf
 		!
 		n_ef	=	size(	fd_distrib	,	1)
 		n_hw 	=	size(	hw_lst		,	1)
 		n_wf	=	size(	en_k		,	1)
-
-
+		!
 		allocate(	df_ln(							n_ef	))
-		allocate(	tmp(			3,3,3,	n_hw			))
 		allocate(	phot_cond(		3,3,3,	n_hw,	n_ef	))
 		phot_cond	=	0.0_dp
 		!
 		!
 		!$OMP  PARALLEL DO DEFAULT(NONE)  																&
-		!$OMP PRIVATE(n, l,  dE_nm,df_ln, dE_nl, c, b, vvv_nl_lm_mn, tmp, hw, omega, dE_nl_hw, ef_idx)	&
+		!$OMP PRIVATE(n, l,  dE_nm,df_ln, dE_nl, c, b, vvv_nl_lm_mn, hw, omega, dE_nl_hw, ef_idx)		&
 		!$OMP SHARED(n_wf, n_hw, n_ef, en_k, fd_distrib, V_ka, hw_lst, phi_laser, i_eta_smr) 			&
 		!$OMP REDUCTION(+: phot_cond)
 		do 	n = 1, n_wf
@@ -48,7 +46,7 @@ contains
 				do l = 1, n_wf
 					if(l==n) cycle
 					!
-					df_ln	=	fd_distrib(:,l)	-	fd_distrib(:,n)
+					df_ln(:)=	fd_distrib(:,l)	-	fd_distrib(:,n)
 					dE_nl	=	cmplx(	en_k(n) - 	 en_k(l),	0.0_dp, dp)		-	 i_eta_smr
 					!
 					!	LOOP DIRECTIONS
@@ -58,31 +56,27 @@ contains
 						end do
 					end do
 					!
-					!	LOOP FREQUENCIES
-					tmp =	0.0_dp
-					do hw = 1, n_hw
-						do omega = -1, 1, 2
-							dE_nl_hw	=	dE_nl +	cmplx(	real(omega,dp)*hw_lst(hw),	0.0_dp,	dp) 		
-							!
-							tmp(:,:,:,hw)	=	tmp(:,:,:,hw)	+	real(	phi_laser *	vvv_nl_lm_mn(:,:,:)	/ ( dE_nm * dE_nl_hw ) ,dp)			
-						end do
-					end do
-					!
 					!	LOOP FERMI LEVEL
-					do ef_idx = 1, n_ef
-						phot_cond(:,:,:,:,ef_idx)		=	phot_cond(:,:,:,:,ef_idx)	+	tmp(:,:,:,:)	* df_ln(ef_idx)
-					end do
+					do ef_idx =1, n_ef
+						!	LOOP FREQUENCIES
+						do hw = 1, n_hw
+							do omega = -1, 1, 2
+								dE_nl_hw	=	dE_nl +	cmplx(	real(omega,dp)*hw_lst(hw),	0.0_dp,	dp) 		
+								!
+								phot_cond(:,:,:,hw,ef_idx)	=	phot_cond(:,:,:,hw,ef_idx)							&
+									+	real(	phi_laser *	vvv_nl_lm_mn(:,:,:)	/ ( dE_nm * dE_nl_hw ) 		,dp)	&
+									/ 	hw_lst(hw)**2		
+							end do
+						end do
+						phot_cond(:,:,:,:,ef_idx)		=	phot_cond(:,:,:,:,ef_idx)	*	df_ln(ef_idx)
+					end do 
+					!
 					!
 				end do
 			end do
 		end do
 		!$OMP END PARALLEL DO
 		!
-		do ef_idx = 1, n_ef
-			do hw = 1, n_hw
-				phot_cond(:,:,:,hw, ef_idx)		=	phot_cond(:,:,:,hw,ef_idx)	/	hw_lst(hw)**2
-			end do
-		end do
 		!
 		return
 	end function
