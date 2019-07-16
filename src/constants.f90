29b94e9131e89f72c378a4dde3f8796e2448781f
Compiler-max <max.merte@gmail.com>
2019-07-16 17:08:40 +0200

    FEATURE initial commit of the "single weyl node" k_model_ham

--- src/constants.f90
+++ src/constants.f90
@@ -12,6 +12,8 @@ module constants
 	!MATHEMATICAL
 	real(dp), 		parameter 	::	PI_dp 			= 4 * atan (1.0_dp)
 	complex(dp),	parameter 	::	i_dp 			= cmplx(0.0_dp, 1.0_dp, dp)
+	complex(dp), 	parameter	::	czero 			= cmplx(0.0_dp,0.0_dp,dp)
+	complex(dp), 	parameter	::	r_one 			= cmplx(1.0_dp,0.0_dp,dp)
 	!		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	!
 	!PHYSICAL
@@ -21,5 +23,11 @@ module constants
 	real(dp),		parameter	::	speedOfLight	= 137.035999_dp 				!	in atomic units
 	real(dp),		parameter	::	kBoltz_Eh_K		= 8.6173303_dp * 1e-5_dp / aUtoEv 		!kBoltz from wiki: kB	=	8.6173303(50)×10−5	eV⋅K−1
 	real(dp),		parameter	::	T_room			= 300.0_dp
+	!
+	complex(dp), dimension(2,2,3)	:: pauli_vec  = reshape(  (/&	
+																czero,	 r_one,	 r_one, 	 czero,	&	!sigma_x 
+																czero,	  i_dp,	 -i_dp ,	 czero, &	!sigma_y
+																r_one,	 czero,	 czero,		cmplx(-1.0_dp,0.0_dp,dp)  &	!sigma_z   
+														/)	,shape=(/2,2,3/))
 	!		~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 end module constants
--- src/model_hams.f90
+++ src/model_hams.f90
@@ -1,6 +1,6 @@
 module model_hams
 	use omp_lib
-	use constants,		only:		dp, i_dp,	&
+	use constants,		only:		dp, i_dp, pauli_vec,	&
 									aUtoEv, aUtoAngstrm
 	use mpi_community,	only:		mpi_root_id, mpi_id, mpi_nProcs, ierr
 	use m_config
@@ -12,15 +12,29 @@ module model_hams
 	public					::		model_ham_allocator,	get_kspace_ham
 	!
 	save
-	integer,	parameter	::		n_free_e_gas=2,	&	
-									n_rashba=2,		&
-									id_rashba=0,	&
-									n_rand=500,		&
+									
+	integer,	parameter	::		id_rashba=0,	&
 									id_rand=1,		&
+									id_weyl=2,		&		
+									!
+									n_free_e_gas=2,	&	
+									n_rashba=2,		&
+									n_rand=500,		&
+									n_weyl=2,		&
 									x=1, y=2, z=3		
 	!		RASHBA PARAMETER
 	real(dp)					::	rashba_a, alpha_R, rashba_Vex
 	real(dp)					::	rashba_nMag(3)
+	!		WEYL PARAMETERS
+	real(dp)					::	weyl_v0, weyl_uz
+	!
+
+	!(/	pauli_0,&
+	!													pauli_x,&
+	!													pauli_y,&
+	!													pauli_z	&
+	!												/)
+	
 	!
 	contains
 !^
@@ -38,11 +52,14 @@ module model_hams
 			case(id_rashba)
 				allocate(		e_k(n_rashba)		)
 				call read_rashba_config(rashba_a, rashba_Vex, rashba_nMag)
+			case(id_rand)
+				allocate(		e_k(n_rand)			)
+			case(id_weyl)
+				allocate(		e_k(n_weyl)			)
+				call read_weyl_config( weyl_v0, weyl_uz)
 			! ***PUT ADDITIONAL MODELS HERE***
 			!case(id_new_case)
-			!	allocate(	e_k(n_new_case)		)
-			case(id_rand)
-				allocate(	e_k(n_rand))
+			!	allocate(		e_k(n_new_case)		)
 			!~
 			case default
 				allocate(		e_k(n_free_e_gas)	)
@@ -64,12 +81,14 @@ module model_hams
 		select case(model_id)
 			case(id_rashba)
 				call	rashba_ex_model(	kpt_idx, kpt, 		H_k, H_ka)	
+			case(id_rand)
+				call	random_mat(			kpt_idx, kpt,		H_k, H_ka)
+			case(id_weyl)
+				call	weyl_node_model(	kpt_idx, kpt,		H_k, H_ka)
+			!~
 			! ***PUT ADDITIONAL MODELS HERE***
 			!case(id_new_case)
 			!	call	new_model(	kpt_idx, kpt,		H_k, H_ka)
-			case(id_rand)
-				call	random_mat(			kpt_idx, kpt,		H_k, H_ka)
-			!~
 			case default
 				call	free_e_gas(			kpt_idx, kpt,		H_k, H_ka)
 		end select
@@ -82,6 +101,9 @@ module model_hams
 !
 !PRIVATE	-	MODEL SYSTEMS
 !
+	!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	!		FREE E-GAS (CONTINUOUS)
+	!~~~
 	subroutine free_e_gas(	kpt_idx, kpt, H_k, H_ka)
 		integer,		intent(in)						::	kpt_idx
 		real(dp),		intent(in)						::	kpt(3)
@@ -105,9 +127,13 @@ module model_hams
 		!
 		return
 	end subroutine
+	!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	!~
 	!~
 	!~
+	!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	!		RASHBA MODEL (CONTINUOUS)
+	!~~~
 	subroutine read_rashba_config(aR, Vex, nMag)
 		real(dp),		intent(out)	::	aR, Vex 
 		real(dp),		intent(out)	::	nMag(3)
@@ -142,14 +168,12 @@ module model_hams
 		!	UNIT CONVERSION
 		aR		=	aR		/	(aUtoEv*aUtoAngstrm)
 		Vex		=	Vex		/	aUtoEv
-		!
-		
 		!~
 		!~
 		return
 	end subroutine
-
-	!
+	!~
+	!~
 	subroutine rashba_ex_model(	kpt_idx, kpt, H_k, H_ka)
 		integer,		intent(in)			::	kpt_idx
 		real(dp),		intent(in)			::	kpt(3)
@@ -157,7 +181,6 @@ module model_hams
 		complex(dp)										::	H_diago
 		real(dp)										::	V_exxx
 		integer											::	spin, xi, up=1, dw=2
-		logical,		save							::	printed
 		!
 		!
 		allocate(	H_k(	n_rashba	 ,	n_rashba	))
@@ -201,6 +224,13 @@ module model_hams
 	!~
 	!~
 	!~
+	!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	!~
+	!~
+	!~
+	!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	!		LARGE RANDOM MATRIX (HERMITIAN)
+	!~~~
 	subroutine random_mat(	kpt_idx, kpt, H_k, H_ka)
 		integer,		intent(in)			::	kpt_idx
 		real(dp),		intent(in)			::	kpt(3)
@@ -237,15 +267,93 @@ module model_hams
 		!
 		return
 	end subroutine
+	!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	!~
+	!~
+	!~
+	!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	!		SINGLE WEYL NODE (CONTINUOUS)
+	!~~~
+	subroutine read_weyl_config(	weyl_v0, weyl_uz)
+		real(dp),			intent(out)		::	weyl_v0, weyl_uz
+		logical								::	input_exist
+		type(CFG_t) 						:: 	weyl_cfg
+		character(len=15)					::	fname="./weyl_node.cfg"
+		!
+		!	INIT
+		weyl_v0		=	0.0_dp
+		weyl_uz 	=	0.0_dp
+		!
+		!	READ CONFIG
+		inquire(file=fname,exist=input_exist)
+		if( input_exist)	then		
+			!OPEN FILE
+			call CFG_read_file(weyl_cfg,fname)
+			!
+			call CFG_add_get(weyl_cfg,	"weyl_node%v0"	,	weyl_v0			,	"(fermi) velocity in eV Ang")
+			call CFG_add_get(weyl_cfg,	"weyl_node%uz"	,	weyl_uz 		,	"exchange coupling in eV"	)
+			!
+			!
+			write(*,'(a,i5.5,a,f6.3,a,f6.3,a)')	"[#",mpi_id,";read_weyl_config]:	SUCCESS (	v0=",	&
+												weyl_v0,	" (eV Ang);	U_z=",weyl_uz," (eV); "
+		else
+			write(*,'(a,i5.5,a)')	"[#",mpi_id,";read_weyl_config]:	WARNING could not find ./weyl_node.cfg , will use standard rashba model"
+		end if
+		!
+		!	UNIT CONVERSION 
+		weyl_v0		=	weyl_v0		/	(aUtoEv*aUtoAngstrm)
+		weyl_uz		=	weyl_uz		/	aUtoEv
+		!~
+		!~
+		return
+	end subroutine	
+	!~
+	!~
+	subroutine weyl_node_model(	kpt_idx, kpt,		H_k, H_ka)
+		integer,						intent(in)			::	kpt_idx
+		real(dp),						intent(in)			::	kpt(3)
+		complex(dp),	allocatable, 	intent(inout)		::	H_k(:,:), H_ka(:,:,:)
+		complex(dp)											::	u2_id(2,2)=reshape((/1.0_dp,0.0_dp,0.0_dp,1.0_dp/),shape=(/2,2/))
+		integer												::	a
+		!
+		allocate(		H_k(	n_weyl, n_weyl	))
+		allocate(		H_ka(3,	n_weyl, n_weyl	))
+		H_k		=	0.0_dp		
+		H_ka	=	0.0_dp
+		!
+		!	SPIN-MOMENTUM LOOKING
+		do a = 1,3
+			H_k(:,:)	=	H_k(:,:)	+	weyl_v0	* kpt(a) * 	pauli_vec(:,:,a)
+			H_ka(a,:,:)	=	H_ka(a,:,:)	+	weyl_v0 	*		pauli_vec(:,:,a)
+		end do
+		!	EXCHANGE
+		H_k(:,:)		=	H_k(:,:)	+	weyl_uz * kpt(3) * 	u2_id(:,:)		
+		H_ka(3,:,:)		=	H_ka(3,:,:)	+	weyl_uz		*		u2_id(:,:)	
+		!
+		!	PRINT DEBUG
+		!write(*,*)		"weyl_node_model kpt(#",kpt_idx,")=",kpt,"	:::"
+		!call print_cmat_u2(H_k(:,:))
+		!~
+		!~
+		return
+	end subroutine
 
 
+	subroutine print_cmat_u2(mat)
+		complex(dp),	intent(in)	::	mat(:,:)
+		integer	:: row, clm
+		do row=1,size(mat,1)
+			do clm = 1, size(mat,2)
+				if(clm==size(mat,2)) then
+					write(*,'(a,f8.3,a,f8.3,a)')				"(",real(mat(row,clm),dp),"+i*",aimag(mat(row,clm)),")   "
+				else
+					write(*,'(a,f8.3,a,f8.3,a)',advance="no")	"(",real(mat(row,clm),dp),"+i*",aimag(mat(row,clm)),")   "
+				end if
+			end do
+		end do
 
-
-
-
-
-
-
+		return
+	end subroutine
 
 	!~
 	!~
