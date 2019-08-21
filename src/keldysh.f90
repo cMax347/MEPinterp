08de98a522d89f6fe2eb83e8cafb96dcbc49d987
Compiler-max <max.merte@gmail.com>
2019-08-21 11:38:21 +0200

    quick

--- src/keldysh.f90
+++ src/keldysh.f90
@@ -6,15 +6,16 @@ module keldysh
 	use input_paras,	only:	kubo_tol
 	use constants,		only:	dp, i_dp, pi_dp
 	use statistics,		only:	fd_stat
-
+	use matrix_math,	only:	mat_tr
 
 	implicit none
 
 
 	private
-	public			::		keldysh_scnd_photoC
+	public			::		keldysh_scnd_photoC, keldysh_scnd_photoC_NUMERICAL
 
 	real(dp),	parameter		::	pi_half	= pi_dp / 2.0_dp
+	integer,	parameter		::	N_energy_int	=	10
 contains
 
 
@@ -65,7 +66,7 @@ function keldysh_scnd_photoC(	en_k, V_ka, hw_lst, smr_lst, ef_lst)			result(phi)
 									-		v_ijk(:,:,:)																							&
 										* en_int_RRA(	en_k(a),	en_k(b)-hw_lst(hw)		,en_k(c),		ef_lst(ef)			, smr_lst(smr))		&
 									!~~~~~~
-									+		v_ikj(:,:,:)																					&
+									+		v_ikj(:,:,:)																							&
 										* en_int_RRR(	en_k(a),	en_k(b)+hw_lst(hw)		,en_k(c),		ef_lst(ef)			, smr_lst(smr))		&
 									!~~~~~~
 									-		v_ikj(:,:,:)																							&
@@ -92,6 +93,162 @@ function keldysh_scnd_photoC(	en_k, V_ka, hw_lst, smr_lst, ef_lst)			result(phi)
 	!
 	return
 end function
+!
+!
+
+
+
+
+function keldysh_scnd_photoC_NUMERICAL(	en_k, U_k, V_ka, hw_lst, smr_lst, ef_lst)			result(phi)
+	real(dp),		intent(in)		::	en_k(:), hw_lst(:), smr_lst(:), ef_lst(:)
+	complex(dp),	intent(in)		::	U_k(:,:), V_ka(:,:,:)
+	complex(dp),	allocatable		::	phi(:,:,:,:,:,:), G_base(:,:,:), tmp(:,:), G_R(:,:), G_R_shift(:,:),G_A(:,:)
+	integer							::	n, ef, int_point, smr, hw, k, j, i 
+	real(dp)						::	epsilon
+	!
+	allocate(	phi(	3,3,3,	size(hw_lst), size(smr_lst), size(ef_lst)		))
+	phi	= cmplx(0.0_dp,0.0_dp,dp)	
+	!
+	allocate(	G_base(			size(U_k,1),size(U_k,2),	size(en_k,1)	))
+	allocate(	G_R(			size(U_k,1),size(U_k,2)						))
+	allocate(	G_R_shift(		size(U_k,1),size(U_k,2)						))	
+	allocate(	tmp(			size(U_k,1),size(U_k,2)						))
+	!
+	!	get |kn><kn| Matrix	  
+	do n = 1, size(U_k,1)
+		G_base(:,:,n)	=	matmul(U_k(:,n:n),U_k(n:n,:))
+	end do
+	!
+	!
+	do ef = 1, size(ef_lst)	
+		do int_point = 1, N_energy_int
+			epsilon	=	real(int_point,dp)		!	TODO!!!!!!!!!!!!!!!!!!!!!111!!!!1!!!!!!!1!!!!!1!!!!!!
+			!
+			!
+			!	TREADS I-IV (	all terms with f(epislon) prefactor )
+	 		if(epsilon< ef_lst(ef))	then
+	 			do smr = 1 , size(smr_lst)
+	 				G_R			=	get_Green_retarded(			epsilon			, 	en_k, G_base, smr_lst(smr)	)
+	 				G_A			=	conjg(transpose(G_R))
+	 				!
+	 				!	I & II		 contribution
+					do hw =1, size(hw_lst)
+	 					G_R_shift	=	get_Green_retarded(		epsilon-hw_lst(hw)	,	en_k, G_base, smr_lst(smr)	)
+	 					do k =1,3
+	 						do j=1,3
+	 							do i=1,3
+	 								tmp(:,:)	=	velo_propagator(			i,j,k,			V_ka, G_R, G_R_shift)
+	 								!
+	 								phi(i,j,k,hw,smr,ef)	=		phi(i,j,k,hw,smr,ef)	+	mat_tr(	matmul(	tmp, 	G_R	)	)
+	 								phi(i,j,k,hw,smr,ef)	=		phi(i,j,k,hw,smr,ef)	-	mat_tr( matmul(	tmp, 	G_A	)	)
+	 							end do
+	 						end do
+	 					end do
+	 				end do
+	 				!
+					!	III & IV		 contribution
+	 				do hw =1, size(hw_lst)
+						G_R_shift	=	get_Green_retarded(		epsilon+hw_lst(hw),		en_k, G_base, smr_lst(smr)	)
+						do k =1,3
+	 						do j=1,3
+	 							do i=1,3
+	 								tmp(:,:)	=	velo_propagator(			i,k,j,			V_ka, G_R, G_R_shift)
+	 								!
+	 								phi(i,j,k,hw,smr,ef)	=		phi(i,j,k,hw,smr,ef)	+	mat_tr(	matmul(	tmp, 	G_R	)	)	
+	 								phi(i,j,k,hw,smr,ef)	=		phi(i,j,k,hw,smr,ef)	-	mat_tr(	matmul(	tmp, 	G_A	)	)
+	 							end do
+	 						end do
+	 					end do
+	 				end do
+	 			end do
+	 		end if
+	 		!
+	 		!
+			do smr = 1 , size(smr_lst)
+	 			G_R			=	get_Green_retarded(			epsilon			, 	en_k, G_base, smr_lst(smr)	)
+	 			G_A			=	conjg(transpose(G_R))
+	 			!
+	 			do hw =1, size(hw_lst)
+	 				!
+	 				!	TREADS V (	f(epislon-hw) term )
+					if(	epsilon-hw_lst(hw) < ef_lst(ef)) then						
+	 					G_R_shift	=	get_Green_retarded(		epsilon-hw_lst(hw),		en_k, G_base, smr_lst(smr)	)
+	 					do k =1,3
+	 						do j=1,3
+	 							do i=1,3
+									tmp(:,:)				=	velo_propagator(	i,j,k,			V_ka, G_R, G_R_shift)
+									!
+									phi(i,j,k,hw,smr,ef)	=	phi(i,j,k,hw,smr,ef)	+		mat_tr(matmul(	tmp, 	G_A	))
+								end do	
+							end do
+						end do
+	 				end if
+	 				!
+	 				!	TREADS VI (	f(epislon+hw) term )
+	 				if(	epsilon+hw_lst(hw) < ef_lst(ef)) then
+	 					G_R_shift	=	get_Green_retarded(		epsilon+hw_lst(hw),			 en_k, G_base, smr_lst(smr)	)
+						do k =1,3
+	 						do j=1,3
+	 							do i=1,3
+									tmp(:,:)	=	velo_propagator(			i,k,j,						V_ka, G_R, G_R_shift	)
+									!
+									phi(i,j,k,hw,smr,ef)	=		phi(i,j,k,hw,smr,ef)	+	mat_tr(matmul(	tmp, 	G_A	))
+								end do
+							end do
+						end do	
+	 				end if
+	 			end do
+	 		end do
+	 		!
+	 		!
+	 	end do
+	end do
+	!
+	!	NORMALIZE ENERGY INTEGRATION (TODO: DO WE NEED MORE HERE)
+	phi	=	phi	/	N_energy_int
+
+	!
+	!
+end function
+
+
+pure function velo_propagator(a,b,c,	V_ka, G_R, G_R_shift)	result(V_abc)
+	integer,		intent(in)			::	a,b,c
+	complex(dp),	intent(in)			::	V_ka(:,:,:), G_R(:,:), G_R_shift(:,:)
+	complex(dp)	,	allocatable			::	V_abc(:,:)
+	!
+	allocate(	V_abc(	size(G_R,1),size(G_R,2)		))
+	!
+	V_abc(:,:)	=	matmul(	V_ka(a,:,:),	G_R(:,:)		)
+	V_abc(:,:)	=	matmul(	V_abc(:,:),		V_ka(b,:,:)		)
+	V_abc(:,:)	=	matmul(	V_abc(:,:),		G_r_shift(:,:)	)
+	V_abc(:,:)	=	matmul(	V_abc(:,:),		V_ka(c,:,:)		)	
+	!
+	return
+end function	
+
+
+
+
+
+
+pure function get_Green_retarded(	epsilon, en_k, G_base, 	smr_val)		result(G_R)
+	real(dp),		intent(in)		::	epsilon, en_k(:), smr_val
+	complex(dp),	intent(in)		::	G_base(:,:,:)
+	complex(dp),	allocatable		::	G_R(:,:)
+	integer							::	n
+	!
+	allocate(	G_R(	size(G_base,1),size(G_base,2)		))
+				G_R	=	cmplx(0.0_dp,0.0_dp,dp)
+	!
+	!
+	do n = 1, size(en_k,1)
+		G_R(:,:)	=		G_R(:,:)	+	G_base(:,:,n)	/	(epsilon - en_k(n) + i_dp*smr_val)
+	end do
+	!
+	return
+end function
+
 
 
 
@@ -213,23 +370,25 @@ complex(dp) pure  function en_int_DGN_RRR(E1, E3, E4, smr)
 	dE_13		=	E3-E1
 	denom_14	=	(E4-E1)**2 + smr**2
 	!
-	re_RRR		=	0.5_dp * 	log( 	( smr**2 + (E3 - E4)**2	)	/	( smr**2 + (E1 - E4)**2 ))			&	
-					- 	dE_13 *(E4-E1)	/ denom_14							
 	!
-	im_RRR		=	-atan((E4-E3)/smr)	&
-					+atan((E4-E1)/smr)	& 
-					-smr * dE_13 / denom_14	
-	!
-	re_RRR		=	re_RRR	/	dE_13**2
-	im_RRR		=	im_RRR	/	dE_13**2
-	!
-	en_int_DGN_RRR	=	cmplx(	re_RRR,	im_RRR,		dp)
+	!!
+	!re_RRR		=	0.5_dp * 	log( 	( smr**2 + (E3 - E4)**2	)	/	( smr**2 + (E1 - E4)**2 ))			&	
+	!				- 	dE_13 *(E4-E1)	/ denom_14							
+	!!
+	!im_RRR		=	-atan((E4-E3)/smr)	&
+	!				+atan((E4-E1)/smr)	& 
+	!				-smr * dE_13 / denom_14	
+	!!
+	!re_RRR		=	re_RRR	/	dE_13**2
+	!im_RRR		=	im_RRR	/	dE_13**2
+	!!
+	!en_int_DGN_RRR	=	cmplx(	re_RRR,	im_RRR,		dp)
 	!
 	!^^^^^^^^^^^^^^^^^^^^^^^^
-	!en_int_DGN_RRR	=	(		0.5_dp * 	log( 	( smr**2 + (E3 - E4)**2	)	/	( smr**2 + (E1 - E4)**2 ))			&	
-	!						- 	dE_13 *(E4-E1)	/ denom_14																&
-	!					)/ dE_13**2																						&
-	!					-	i_dp   *(	atan((E4-E3)/smr)	-	atan((E4-E1)/smr) + smr* dE_13 / denom_14 )	/ dE_13**2		
+	en_int_DGN_RRR	=	(		0.5_dp * 	log( 	( smr**2 + (E3 - E4)**2	)	/	( smr**2 + (E1 - E4)**2 ))			&	
+							- 	dE_13 *(E4-E1)	/ denom_14																&
+						)/ dE_13**2																						&
+						-	i_dp   *(	atan((E4-E3)/smr)	-	atan((E4-E1)/smr) + smr* dE_13 / denom_14 )	/ dE_13**2		
 	!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	!
 	return
